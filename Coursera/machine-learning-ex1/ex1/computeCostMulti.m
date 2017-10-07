9ff34483306a6eb4ab72b3a23eb022beae60f375
Sangwon Hong <qpakzk@gmail.com>
2017-10-08 00:13:23 +0900

    gradientDescentMulti.m
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- Coursera/machine-learning-ex1/ex1/computeCostMulti.m
+++ Coursera/machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,9 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+hypothesis = X * theta;
+J = sum((hypothesis - y) .^ 2) / (2 * m);
 
 % =========================================================================
 
--- Coursera/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ Coursera/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,16 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
-
+    hypothesis = X * theta;
+    theta = theta - alpha * X' * (hypothesis - y) / m;
     % ============================================================
 
     % Save the cost J in every iteration    
